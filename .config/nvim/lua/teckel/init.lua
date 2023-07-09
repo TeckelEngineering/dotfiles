e8fc68fb29cdd6d834371262779dae366c9ef950
TeckelEngineering <b.boisvert@protonmail.com>
2023-07-09 17:33:52 +0000

    Initial Commit

---
+++ .config/nvim/lua/teckel/init.lua
@@ -0,0 +1,12 @@
+require("teckel.remap")
+require("teckel.treesitter")
+require("teckel.harpoon")
+require("teckel.fugitive")
+require("teckel.undotree")
+require("teckel.lsp")
+require("teckel.set")
+vim.cmd("colorscheme tokyonight-storm")
+require("lualine").setup()
+
+
+
